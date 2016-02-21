f82f6e858ee48f5ed4e9a98db49299daa20040d6
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-02-20 22:00:18 -0500

    Merge branch 'master' of https://github.com/alejoperez/Cloud-Design
    Conflicts:
    	db.sqlite3

--- hello/static/src/app.js
+++ hello/static/src/app.js
@@ -17,7 +17,7 @@
 
         $routeProvider
             .when('/main', {
-                templateUrl: 'static/src/modules/manager/main.tpl.html',
+                templateUrl: 'static/src/modules/main/main.tpl.html',
                 controller: 'managerCtrl',
                 controllerAs: 'ctrl'
             })
