29e9f8bd37bbfac68ec13014eabe296ddf6ccecb
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-02-21 14:07:18 -0500

    Admin profile

--- hello/admin.py
+++ hello/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import Design
 #from .models import Independent, Job, Comment
 
 # Register your models here.
-#admin.site.register(Independent)
+admin.site.register(Design)
 #admin.site.register(Job)
 #admin.site.register(Comment)
