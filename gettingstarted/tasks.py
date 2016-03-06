73cebc4348bfa6275f36efdd11eef39d7c0e2357
mslancheros10 <ms.lancheros10@uniandes.edu.co>
2016-03-06 17:08:48 -0500

    Imports tasks.py fixed

--- gettingstarted/tasks.py
+++ gettingstarted/tasks.py
@@ -1,6 +1,6 @@
 import celeryApp
 from hello.models import Administrator, Design, Designer
-from hello.src import ImageUtils, EmailUtils
+from hello.src import imageUtils, emailUtils
 
 
 @celeryApp.app.task
@@ -8,7 +8,7 @@ def updateInProgressDesigns():
     designQS = Design.objects.filter(status=1)
     designList = list(designQS)
     for design in designList:
-        ImageUtils.resizeImage(design)
+        imageUtils.resizeImage(design)
         design.status=2
         design.save()
-        EmailUtils.sendMailImageProccesed(design.designer.email)
+        emailUtils.sendMailImageProccesed(design.designer.email)
