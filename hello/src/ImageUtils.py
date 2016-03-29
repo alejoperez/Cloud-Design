cab3ac2ce43bdbd94b0f6ef2151bac645f06a1bf
unknown <if.garcia11@uniandes.edu.co>
2016-03-28 21:22:20 -0500

    Cambio imagen

--- hello/src/ImageUtils.py
+++ hello/src/ImageUtils.py
@@ -6,12 +6,15 @@ from PIL import ImageDraw
 from hello.models import Design
 import urllib, cStringIO
 
+Image.LOAD_TRUNCATED_IMAGES = True
+
 def resizeImage(design):
     #buff = StringIO.StringIO()
     #buff.write(open(str('https://s3-us-west-2.amazonaws.com/cloud-images-andes/'+design.imageFile), 'rb').read())
     #image = Image.open(buff)
     file = cStringIO.StringIO(urllib.urlopen('https://s3-us-west-2.amazonaws.com/cloud-images-andes/'+str(design.imageFile)).read())
     image = Image.open(file)
+
     #image = image.resize((800, 600), Image.ANTIALIAS)
     draw = ImageDraw.Draw(image)
     #font = ImageFont.truetype("arial.ttf", 15)
