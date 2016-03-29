a7f3f6f0e9a73341576b428bc7221f397e83de24
unknown <if.garcia11@uniandes.edu.co>
2016-03-28 21:08:16 -0500

    retrevie image

--- hello/src/ImageUtils.py
+++ hello/src/ImageUtils.py
@@ -4,11 +4,14 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 from hello.models import Design
+import urllib, cStringIO
 
 def resizeImage(design):
-    buff = StringIO.StringIO()
-    buff.write(open(str('https://s3-us-west-2.amazonaws.com/cloud-images-andes/'+design.imageFile), 'rb').read())
-    image = Image.open(buff)
+    #buff = StringIO.StringIO()
+    #buff.write(open(str('https://s3-us-west-2.amazonaws.com/cloud-images-andes/'+design.imageFile), 'rb').read())
+    #image = Image.open(buff)
+    file = cStringIO.StringIO(urllib.urlopen('https://s3-us-west-2.amazonaws.com/cloud-images-andes/'+str(design.imageFile)).read())
+    image = Image.open(file)
     image = image.resize((800, 600), Image.ANTIALIAS)
     draw = ImageDraw.Draw(image)
     #font = ImageFont.truetype("arial.ttf", 15)
