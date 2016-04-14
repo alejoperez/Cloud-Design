1df5fb6069f180dba6ced56f7c205cd4b2e101ba
Alejandro Perez <alejoperezgarcia@gmail.com>
2016-04-14 13:07:38 -0500

    procfile

--- worker.sh
+++ worker.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 # Hello World Program in Bash Shell
 
 echo "Iniciando worker"
-celery -A gettingstarted beat --app=gettingstarted.celeryApp:app -l debug --max-interval=120
-echo "Iniciando celery getting started"
 python manage.py celeryd -l info
 echo "Iniciando python manage.py celery info"
+celery -A gettingstarted beat --app=gettingstarted.celeryApp:app -l debug --max-interval=120
+echo "Iniciando celery getting started"
