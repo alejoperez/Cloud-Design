01e6f34056156f88336abb765104b146ffec846a
Alejandro Perez <alejoperezgarcia@gmail.com>
2016-04-14 13:25:41 -0500

    sh

--- worker.sh
+++ worker.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Hello World Program in Bash Shell
 
-echo "Iniciando worker"
-python manage.py celeryd -l info
-echo "Iniciando python manage.py celery info"
-celery -A gettingstarted beat --app=gettingstarted.celeryApp:app -l debug --max-interval=120
-echo "Iniciando celery getting started"
+celery -A gettingstarted beat --app=gettingstarted.celeryApp:app -l debug --max-interval=120 & python manage.py celeryd -l info
